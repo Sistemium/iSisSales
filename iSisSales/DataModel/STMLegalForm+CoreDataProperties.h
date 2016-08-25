d1a1ce990c1dcd3307006b0fc4c8c03e1c7fb3ad
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-08-25 14:36:32 +0300

    add legalForm.ord

--- iSisSales/DataModel/STMLegalForm+CoreDataProperties.h
+++ iSisSales/DataModel/STMLegalForm+CoreDataProperties.h
@@ -2,7 +2,7 @@
 //  STMLegalForm+CoreDataProperties.h
 //  iSisSales
 //
-//  Created by Maxim Grigoriev on 17/08/16.
+//  Created by Maxim Grigoriev on 25/08/16.
 //  Copyright © 2016 Sistemium UAB. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -18,15 +18,16 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, nonatomic, retain) NSString *commentText;
 @property (nullable, nonatomic, retain) NSDate *deviceCts;
 @property (nullable, nonatomic, retain) NSDate *deviceTs;
+@property (nullable, nonatomic, retain) NSString *fullName;
 @property (nullable, nonatomic, retain) NSNumber *id;
 @property (nullable, nonatomic, retain) NSNumber *isFantom;
 @property (nullable, nonatomic, retain) NSDate *lts;
+@property (nullable, nonatomic, retain) NSString *name;
 @property (nullable, nonatomic, retain) NSData *ownerXid;
 @property (nullable, nonatomic, retain) NSString *source;
 @property (nullable, nonatomic, retain) NSString *target;
 @property (nullable, nonatomic, retain) NSData *xid;
-@property (nullable, nonatomic, retain) NSString *name;
-@property (nullable, nonatomic, retain) NSString *fullName;
+@property (nullable, nonatomic, retain) NSNumber *ord;
 @property (nullable, nonatomic, retain) NSSet<STMPartner *> *partners;
 
 @end
--- iSisSales/DataModel/STMLegalForm+CoreDataProperties.m
+++ iSisSales/DataModel/STMLegalForm+CoreDataProperties.m
@@ -2,7 +2,7 @@
 //  STMLegalForm+CoreDataProperties.m
 //  iSisSales
 //
-//  Created by Maxim Grigoriev on 17/08/16.
+//  Created by Maxim Grigoriev on 25/08/16.
 //  Copyright © 2016 Sistemium UAB. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,15 +16,16 @@
 @dynamic commentText;
 @dynamic deviceCts;
 @dynamic deviceTs;
+@dynamic fullName;
 @dynamic id;
 @dynamic isFantom;
 @dynamic lts;
+@dynamic name;
 @dynamic ownerXid;
 @dynamic source;
 @dynamic target;
 @dynamic xid;
-@dynamic name;
-@dynamic fullName;
+@dynamic ord;
 @dynamic partners;
 
 @end
