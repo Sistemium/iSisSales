8daab86c46fff9f66ed93e64037577300348fd84
gogbajbobo <grigoriev.maxim@gmail.com>
2017-04-09 11:50:48 +0300

    generate STMPhotoReport class to get NSClassFromString(entityName) in STMCorePhotosController

---
+++ iSisSales/DataModel/STMPhotoReport+CoreDataClass.m
@@ -0,0 +1,16 @@
+//
+//  STMPhotoReport+CoreDataClass.m
+//  iSisSales
+//
+//  Created by Maxim Grigoriev on 09/04/2017.
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
+
+#import "STMPhotoReport+CoreDataClass.h"
+#import "STMOutlet+CoreDataClass.h"
+
+#import "STMSalesman+CoreDataClass.h"
+
+@implementation STMPhotoReport
+
+@end
