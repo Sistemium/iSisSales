b0f17077cfe60ddb5599373b8c39bc3440ec408c
gogbajbobo <grigoriev.maxim@gmail.com>
2017-04-19 18:16:11 +0300

    add TODO: comment

--- iSisSales/STMAppDelegate.m
+++ iSisSales/STMAppDelegate.m
@@ -14,6 +14,9 @@
 
 @implementation STMAppDelegate
 
+//TODO: may be we have to rename project from iSisSales back to iSistemium for example (or create new and move all files to it)
+//      different configurations gives us needed app names/icons like iSisSales, iSisWarehouse, etc.
+//      also we have to consider to get rid of iSisCore and merge separated files/dirs/classes to one
 
 - (void)startAuthController {
     [STMAuthController authController];
